136d2d5ddf5e301fe3e0de672a5f778822fc8575
Metehan GÜNGÖR <102655648+gungorMetehan@users.noreply.github.com>
2026-01-12 23:26:31 +0300

    Update 09-yaygin-hatalar-ve-uyarilar.R

--- 2026-01-12/09-yaygin-hatalar-ve-uyarilar.R
+++ 2026-01-12/09-yaygin-hatalar-ve-uyarilar.R
@@ -1,63 +1,71 @@
 # 9 - YAYGIN HATALAR ve UYARILAR
 ## Hatalar (Errors)
-### Tanımlı Olmayan Sütun
+### Tanimli Olmayan Sutun
 
-veriseti <- data.frame(V1 = c(6, 4, 5, 2, 5),
-                       V2 = c(5, 5, 7, 8, 9),
-                       V3 = c(2, 7, 9, 9, 7))
+veriseti <- data.frame(
+  V1 = c(6, 4, 5, 2, 5),
+  V2 = c(5, 5, 7, 8, 9),
+  V3 = c(2, 7, 9, 9, 7)
+)
 
 veriseti[veriseti$V1 > 4]
 
 veriseti[veriseti$V1 > 4, ]
 
-### Yazım Hataları
+### Yazim Hatalari
 
-# aritmetik ortalama alma işlemi mean() fonksiyonu ile yapılmaktadır
+# aritmetik ortalama alma islemi mean() fonksiyonu ile yapilmaktadir
 maen(c(10, 40, 70))
 
-# oluşturulan data frame'deki değişkenin ismi lengths_A'dır, lenghts_A değil
-boyutlar <- data.frame(lengths_A = c(190, 181, 177, 170, 180),
-                       widths_A = c(120, 115, 120, 130, 140))
+# olusturulan data frame'deki degiskenin ismi lengths_A'dir, lenghts_A degil
+boyutlar <- data.frame(
+  lengths_A = c(190, 181, 177, 170, 180),
+  widths_A  = c(120, 115, 120, 130, 140)
+)
 boyutlar[, lenghts_A]
 
-### if() Kullanımı
+### if() Kullanimi
 
 sayi <- 1:8
-if(sayi > 5) {
-  paste(sayi, "5'ten büyüktür.")
+if (sayi > 5) {
+  paste(sayi, "5'ten buyuktur.")
 }
 
 sayi <- 1:8
-ifelse(sayi > 5,
-       paste(sayi, "sayisi 5'ten büyüktür."),
-       paste(sayi, "sayisi 5'ten büyük değildir."))
+ifelse(
+  sayi > 5,
+  paste(sayi, "sayisi 5'ten buyuktur."),
+  paste(sayi, "sayisi 5'ten buyuk degildir.")
+)
 
-### Hatalı Argüman Kullanımı
+### Hatali Arguman Kullanimi
 
 rnorm(100, ss = 5)
 
-### İndirilmemiş Paket
+### Indirilmemis Paket
 
 library(ltm)
 
-## Uyarılar (Warnings)
-### Farklı Uzunluklardaki Vektörler
+## Uyarilar (Warnings)
+### Farkli Uzunluklardaki Vektorler
 
 dizi_A <- 3:11
 dizi_B <- 2:15
 
-# toplama işlemi gerçekleşecektir. 
-# Ancak dizi_A'nın son elemanı (11), dizi_B'nin 9. elemanı (10) ile toplandıktan sonra
-# dizi_B'nin 10. elemanı tekrar dizi_A'nın ilk elemanı ile toplanarak işlem devam eder.
+# toplama islemi gerceklesecektir.
+# Ancak dizi_A'nin son elemani (11), dizi_B'nin 9. elemani (10) ile toplandiktan sonra
+# dizi_B'nin 10. elemani tekrar dizi_A'nin ilk elemani ile toplanarak islem devam eder.
 dizi_A + dizi_B
 
-### Standart Sapmanın Sıfır Oluşu
+### Standart Sapmanin Sifir Olusu
 
-# 24 tane 50 değerinden oluşan bir vektör
+# 24 tane 50 degerinden olusan bir vektor
 sinif_1 <- rep(50, 24)
-# ortalaması 50, standart sapması 5 olan normal dağılımdan seçilen 24 değerin üstündeki 
-# kendine en yakın tam sayıya yuvarlanmış halinden oluşan, uzunluğu 24 olan bir vektör
+
+# ortalamasi 50, standart sapmasi 5 olan normal dagilimdan secilen 24 degerin
+# ustundeki kendine en yakin tam sayiya yuvarlanmis halinden olusan,
+# uzunlugu 24 olan bir vektor
 sinif_2 <- ceiling(rnorm(24, mean = 50, sd = 5))
 
-# cor() fonksiyonu ile korelasyonun hesaplanması
+# cor() fonksiyonu ile korelasyonun hesaplanmasi
 cor(sinif_1, sinif_2)
