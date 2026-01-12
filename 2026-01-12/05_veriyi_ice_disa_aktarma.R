9ee8ae0ef5ee77e141c2f0a98f887744b62df865
Metehan GÜNGÖR <102655648+gungorMetehan@users.noreply.github.com>
2026-01-12 22:57:46 +0300

    Update data import/export instructions in R

--- 2026-01-12/05_veriyi_ice_disa_aktarma.R
+++ 2026-01-12/05_veriyi_ice_disa_aktarma.R
@@ -1,43 +1,47 @@
-# 5 - VERİYİ İÇE ve DIŞA AKTARMA
-## İçe Aktarma (Import)
+# 5 - VERIYI ICE ve DISA AKTARMA
+## Ice Aktarma (Import)
 
-# R’da veriler ile çalışmalar yürütmek için verileri içe aktarmak (import) gerekebilir. 
-# Bunun için pek çok alternatif yol bulunmaktadır. Bu eğitimde özellikle iki yol ele alınacaktır. 
-# Bunlardan birincisi RStudio’nun sunduğu bir imkan olan Environment kısmındaki ‘Import Dataset’ menüsü, 
-# diğeri ise fonksiyonlar aracılığı ile içe aktarma olacaktır.
+# R'da veriler ile calismalar yurutmek icin verileri ice aktarmak (import) gerekebilir. 
+# Bunun icin pek cok alternatif yol bulunmaktadir. Bu egitimde ozellikle iki yol ele alinacaktir. 
+# Bunlardan birincisi RStudio'nun sundugu bir imkan olan Environment kismindaki 
+# 'Import Dataset' menusudur, digeri ise fonksiyonlar araciligi ile ice aktarma olacaktir.
 
-TR_university <- read.csv("https://raw.githubusercontent.com/gungorMetehan/TREduData/main/TREduData_university.csv")
+TR_university <- read.csv(
+  "https://raw.githubusercontent.com/gungorMetehan/TREduData/main/TREduData_university.csv"
+)
 
 library(readr)
-TR_university <- read_csv("https://raw.githubusercontent.com/gungorMetehan/TREduData/main/TREduData_university")
+TR_university <- read_csv(
+  "https://raw.githubusercontent.com/gungorMetehan/TREduData/main/TREduData_university"
+)
 
-# Farklı formattaki verilerin içe aktarılması
-# read_csv() - Virgülle ayrılmış değerlerden oluşan CSV dosyaları içindir.
-# read_tsv() - Boşlukla ayrılmış değerlerden (tab-separated values) oluşan dosyalar içindir.
-# read_csv2()	- Noktalı virgülle ayrılmış değerlerden oluşan dosyalar içindir (ondalıklar için virgül kullanılmıştır).
-# read_delim()	- Değerlerin farklı şekillerde birbirinden ayrıldığı özel dosya biçimleri içindir (örneğin |).
-# read_fwf()	- Karakterlerin genişliği üzerinden bir arşivleme yapılan metin dosyaları içindir (örneğin, eski veri setlerinde işe yarayabilir. Bu setlerde, değerler birbirinden ayrılmamıştır ve her sütunun genişliği sabittir. Optik okuyuculardan alınan veriler buna örnek gösterilebilir).
-# read_table() - Veri setindeki değerler boşluklarla birbirinden ayrıldığında kullanılabilir. Fonksiyon, boşlukların başındaki ve sonundaki değerleri göz önünde bulundurarak bu boşlukları silerek hücreleri oluşturur.
+# Farkli formattaki verilerin ice aktarilmasi
+# read_csv()  - Virgul ile ayrilmis degerlerden olusan CSV dosyalari icindir.
+# read_tsv()  - Boslukla ayrilmis degerlerden (tab-separated values) olusan dosyalar icindir.
+# read_csv2() - Noktali virgul ile ayrilmis degerlerden olusan dosyalar icindir (ondaliklar icin virgul kullanilmistir).
+# read_delim()- Degerlerin farkli sekillerde birbirinden ayrildigi ozel dosya bicimleri icindir (ornegin |).
+# read_fwf()  - Karakterlerin genisligi uzerinden arsivleme yapilan metin dosyalari icindir (eski veri setlerinde kullanilabilir).
+# read_table()- Veri setindeki degerler bosluklarla birbirinden ayrildiginda kullanilabilir.
 
 
-## Dışa Aktarma (Export)
+## Disa Aktarma (Export)
 
-# öncelikle iris veri setini çağıralım ve ilk 15 satırından oluşan iris15 isimli yeni bir veri seti oluşturalım.
+# oncelikle iris veri setini cagirip ilk 15 satirindan olusan iris15 isimli yeni bir veri seti olusturalim
 data(iris)
 iris15 <- head(iris, 15)
 
-# write.table() fonksiyonu ile iris15'i bir metin dosyası olarak kaydedelim.
-write.table(iris15, file = 'iris15.txt', col.names = TRUE, row.names = FALSE)
+# write.table() fonksiyonu ile iris15'i bir metin dosyasi olarak kaydedelim
+write.table(iris15, file = "iris15.txt", col.names = TRUE, row.names = FALSE)
 
-# write.table() fonksiyonu ile iris15'i bir CSV dosyası olarak kaydedelim.
-write.table(iris15, file = 'iris15.csv', row.names = FALSE, sep = ",")
+# write.table() fonksiyonu ile iris15'i bir CSV dosyasi olarak kaydedelim
+write.table(iris15, file = "iris15.csv", row.names = FALSE, sep = ",")
 
-# öncelikle iris veri setini çağıralım ve ilk 20 satırından oluşan iris20 isimli yeni bir veri seti oluşturalım.
+# oncelikle iris veri setini cagirip ilk 20 satirindan olusan iris20 isimli yeni bir veri seti olusturalim
 data(iris)
 iris20 <- head(iris, 20)
 
-# readr paketini çalıştıralım (fonksiyonun paketidir)
+# readr paketini calistiralim
 library(readr)
 
-# çıktıyı çalışma dizinine kaydedelim
+# ciktiyi calisma dizinine kaydedelim
 write_csv(iris20, file = "iris20.csv")
