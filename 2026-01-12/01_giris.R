8a4c145fb7d8aee6817d4bbaa1ae8c170e8d563a
Metehan GÜNGÖR <102655648+gungorMetehan@users.noreply.github.com>
2026-01-12 22:22:39 +0300

    Update 01_giris.R

--- 2026-01-12/01_giris.R
+++ 2026-01-12/01_giris.R
@@ -1,39 +1,39 @@
-# 1 - GİRİŞ
+# 1 - GIRIS
 
-# R, harflerin büyüklüğüne küçüklüğüne duyarlıdır. 
-# Yani bir nesnenin adı ‘M’, diğer nesnenin adı ‘m’ ise R, bu iki nesneyi farklı şekilde kaydetmiş olacaktır. 
+# R, harflerin buyuklugune kucuklugune duyarlidir (case-sensitive'dir. 
+# Yani bir nesnenin adi 'M', diger nesnenin adi 'm' ise R, bu iki nesneyi farkli sekilde kaydetmis olacaktir. Iki nesne de Environment kismindan goruntulenebilir.
 
-m <- 2025
+m <- 2026
 M <- 2025
 
-# (diyez) sembolünün bulunduğu satırdaki sembolün sağında kalan kodlar R tarafından çalıştırılmaz. Bu sembol, genellikle yorum, hatırlatıcı eklemek için kullanılır.
+# (diyez) sembolunun bulundugu satirdaki sembolun saginda kalan kodlar R tarafindan calistirilmaz. 
+# Bu sembol, genellikle yorum (comment), hatirlatici eklemek icin kullanilir.
 
-# Satır komutları, yeni satırlar oluşturarak birbirinden ayrılır. 
-# Bunun yanı sıra iki komutun arasına ; sembolü konularak da aynı satırda birden fazla komut çalıştırılabilir. 
-# Ancak genellikle, daha okunabilir satır komutları için komutlar farklı satırlara yazılmaktadır.
+# Satir komutlari, yeni satirlar olusturarak birbirinden ayrilir. 
+# Bunun yani sira iki komutun arasina ; sembolu konularak da ayni satirda birden fazla komut calistirilabilir. 
+# Ancak genellikle, daha okunabilir satir komutlari icin komutlar farkli satirlara yazilmaktadir.
 
-abc <- 100 ; def <- 200
+abc <- 100 ; def <- 200 # Burada iki ayri nesne olusturulmaktadir, bir nesnenin adi abc, digerininki ise def'dir.
 
-# Bir satır komutunu henüz tamamlamadan çalıştırdığınızda konsolda (console) + işareti belirir. 
-# Bu işaret, komutun noksan olduğu anlamına gelir. 
-# Bununla genelde bir fonksiyonun son kapatma parantezinin ‘)’ kullanılmadığı durumlarda karşılaşılır.
+# Bir satir komutunu henuz tamamlamadan calistirdiginizda konsolda (console) + isareti belirir. Bu isaret, komutun noksan oldugu anlamina gelir. 
+# Bununla genelde bir fonksiyonun son kapatma parantezinin ')' kullanilmadigi durumlarda karsilasilir.
 
-# R, satırdaki komutlarda fazladan konulan boşluklara duyarlı değildir. 
-# Yalnızca bir kodun yazımında hatalı bir boşluk bırakıldığında bu fazlalık boşluk sıkıntı yaratabilir. 
+# R, satirdaki komutlarda fazladan konulan bosluklara duyarlidir. Yalnizca bir kodun yaziminda hatali bir bosluk birakildiginda bu fazlalik bosluk sikinti yaratabilir. 
 
 a <- 1 + 2
 b <- 1                 +       2
 
-# R’da aynı işlem birden fazla şekilde gerçekleştirilebilir. 
+# R'da ayni islem birden fazla sekilde gerceklestirilebilir. Ornegin, asagidaki komutlari calistiralim ve ne sonuclar dondugune bakalim.
 
-# R’da bir nesne ismi sayı ile başlamamalıdır. 
-# Nesneler adlandırılırken adlandırmaya harflerle başlanmalı, istenirse sonra rakam, 
-# nokta (.) ya da alt çizgi (_) kullanılabilir. 
-# Tek bir harften oluşan nesneler oluşturulabilse de, nesnelere F ve T harfleri verilmemelidir.
+c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+1:10
+seq(1, 10, 1)
 
+# R'da bir nesne ismi sayi ile baslamamalidir. 
+# Nesneler adlandirilirken adlandirmaya harflerle baslanmali, istenirse sonra rakam, nokta (.) ya da alt cizgi (_) kullanilabilir. 
+# Tek bir harften olusan nesneler olusturulabilse de, nesnelere F ve T harfleri verilmemelidir.
 
-# R, hesap makinesi olarak kullanılabilir. Dört işlem +, -, *, / sembollerinin yardımı ile yapılmaktadır.
-
+# R, hesap makinesi olarak kullanilabilir. Dort islem +, -, *, / sembollerinin yardimi ile yapilmaktadir. Bunu konsol (console) kisminda calistirmak daha uygun olabilir.
 
 # ATAMA
 
@@ -43,8 +43,14 @@ x
 x = c(12, 91, 6, 19)
 x
 
+# Bu dort farkli yol ile atama yapilabiliyor olsa da, yukarida verilen ilk ikisi daha yaygindir.
+
 c(12, 91, 6, 19) -> x
 x
 
 assign("x", c(12, 91, 6, 19))
+
 x
+
+# camel-case nesne isimlendirme: veriSeti, standartSapma, toplamPuan
+# snake-case nesne isimlendirme: uzunluk_a, calisan_maas, personel_id
